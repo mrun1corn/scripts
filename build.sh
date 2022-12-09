007a50fa6c2d556243f4e1930a39f206224f7368
Mr. Unicorn <48880125+mrun1corn@users.noreply.github.com>
2022-12-09 16:11:29 +0600

    Fix typos

--- build.sh
+++ build.sh
@@ -28,10 +28,10 @@ rm -rf hardware/qcom-caf/msm8953/audio
 rm -rf hardware/qcom-caf/msm8953/media
 rm -rf hardware/qcom-caf/msm8953/display
 # Redownload Hals
-https://github.com/LineageOS/android_hardware_qcom_wlan -b lineage-20.0-caf hardware/qcom-caf/wlan
-git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-20.0-caf-msm8953 hardware/qcom-caf/msm8996/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-20.0-caf-msm8953 hardware/qcom-caf/msm8996/media
-git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-20.0-caf-msm8953 hardware/qcom-caf/msm8996/display
+git clone https://github.com/LineageOS/android_hardware_qcom_wlan -b lineage-20.0-caf hardware/qcom-caf/wlan
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-20.0-caf-msm8953 hardware/qcom-caf/msm8953/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-20.0-caf-msm8953 hardware/qcom-caf/msm8953/media
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-20.0-caf-msm8953 hardware/qcom-caf/msm8953/display
 
 #Download device tree
 https://github.com/mrun1corn/device_xiaomi_onclite -b 13 device/xiaomi/onclite
